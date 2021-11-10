9ba514048436126b3b2373a6efcfb83bad5bae0e
Waruna-Kaushalya <waruna95610@gmail.com>
2021-11-11 00:35:39 +0530

    cubit part done. Implemented BlokConsumer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cubit_bloc_tutorial/cubit/weather_cubit.dart';
+import 'package:flutter_cubit_bloc_tutorial/data/weather_repository.dart';
 import 'package:flutter_cubit_bloc_tutorial/pages/weather_search_page.dart';
 
 void main() => runApp(MyApp());
@@ -8,7 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: WeatherSearchPage(),
+      home: BlocProvider(
+        create: (context) => WeatherCubit(
+          FakeWeatherRepository(),
+        ),
+        child: WeatherSearchPage(),
+      ),
     );
   }
 }
--- lib/pages/weather_search_page.dart
+++ lib/pages/weather_search_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cubit_bloc_tutorial/cubit/weather_cubit.dart';
 import 'package:flutter_cubit_bloc_tutorial/data/model/weather.dart';
 
 class WeatherSearchPage extends StatefulWidget {
@@ -16,7 +18,28 @@ class _WeatherSearchPageState extends State<WeatherSearchPage> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 16),
         alignment: Alignment.center,
-        child: buildInitialInput(),
+        child: BlocConsumer<WeatherCubit, WeatherState>(
+          listener: (context, state) {
+            if (state is WeatherError) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(state.message),
+                ),
+              );
+            }
+          },
+          builder: (context, state) {
+            if (state is WeatherInitial) {
+              return CityInputField();
+            } else if (state is WeatherLoading) {
+              return buildLoading();
+            } else if (state is WeatherLoaded) {
+              return buildColumnWithData(state.weather);
+            } else {
+              return buildInitialInput();
+            }
+          },
+        ),
       ),
     );
   }
@@ -72,5 +95,8 @@ class CityInputField extends StatelessWidget {
     );
   }
 
-  void submitCityName(BuildContext context, String cityName) {}
+  void submitCityName(BuildContext context, String cityName) {
+    final weatherCubit = context.read<WeatherCubit>();
+    weatherCubit.getWeather(cityName);
+  }
 }
